59ec5d3e7b9b857531a26763292a824b1897ecc2
Oliver Vukovic <oliver_vukovic@yahoo.com>
2021-12-04 22:38:17 +0100

    basic js

---
+++ main.js
@@ -0,0 +1,29 @@
+// ---- Video
+
+var video = document.getElementById("video");
+var button = document.getElementById("play-button");
+
+function playVideo() {
+        video.play();
+        button.style.display = "none";
+    } 
+
+
+// ---- Specifications
+
+function dropDown(clickedRow) {
+    var dropdownMenu = clickedRow.querySelector('ul')
+    dropdownMenu.classList.toggle('visible')
+}
+
+// click listeners
+let rows = document.querySelectorAll('.row')
+// console.log(rows);
+
+rows.forEach(function(row){
+    row.addEventListener('click', function(){
+        dropDown(row)
+    })
+})
+
+
